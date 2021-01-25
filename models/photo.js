252d81d9e9c9e7c0dccd71c0a95ed7cf0c52d4c2
OlyaKyrylyuk <olya_kyrylyuk15@ukr.net>
2021-01-25 23:21:30 +0200

    prettier changes

--- models/photo.js
+++ models/photo.js
@@ -1,19 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const photoSchema = new mongoose.Schema({
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 
-    img:
-    {
-        data: Buffer,
-        contentType: String
-    },
+  category: {
+    type: String,
+    required: true,
+  },
+});
 
-    category:{
-        type:String,
-        required: true
-    }
-    
-})
-
-  
-module.exports = mongoose.model('Photo', photoSchema)
+module.exports = mongoose.model("Photo", photoSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const Photo = require('../models/photo')
-const path = require('path')
-var fs = require('fs')
+const express = require("express");
+const router = express.Router();
+const Photo = require("../models/photo");
+const path = require("path");
+var fs = require("fs");
 
-var multer = require('multer');
-const uploadPath = path.join('public/uploads')
-const imageMimeTypes = ['image/jpeg', 'image/png']
+var multer = require("multer");
+const uploadPath = path.join("public/uploads");
+const imageMimeTypes = ["image/jpeg", "image/png"];
 /*var storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads')
@@ -21,105 +21,92 @@ const imageMimeTypes = ['image/jpeg', 'image/png']
 const upload = multer({
   dest: uploadPath,
   fileFilter: (req, file, callback) => {
-    callback(null, imageMimeTypes.includes(file.mimetype))
+    callback(null, imageMimeTypes.includes(file.mimetype));
+  },
+});
+
+router.get("/", async (req, res) => {
+  try {
+    Photo.find({}, (err, photos) => {
+      res.render("allPhotos", { photos: photos });
+    });
+  } catch {
+    res.send("error");
   }
-})
+});
 
-router.get('/', async(req,res)=>{
-    try{
-    Photo.find({}, (err, photos) => {   
-        res.render('allPhotos', { photos: photos });
-                  
-     })
-     }
-    catch{
-        res.send("error")
+router.get("/search", async (req, res) => {
+  try {
+    if (req.body.search != null) {
+      photo = await Photo.find().where("category", req.body.search);
+      res.render("allPhotos", { photos: photo });
     }
-   
-})
+  } catch {
+    res.send("error");
+  }
+});
 
-router.get('/search', async(req,res)=>{
-    try{
-        if(req.body.search!=null){
-            photo = await Photo.find().where('category', req.body.search)
-            res.render('allPhotos', {photos:photo})
-        }
-    
-        
-     }
-    catch{
-        res.send("error")
-    }
-   
-})
+router.get("/new", (req, res) => {
+  res.render("newPhoto", { photo: new Photo() });
+});
 
-router.get('/new',(req,res)=>{
-    res.render('newPhoto',{photo: new Photo()})
-})
+router.post("/new", upload.single("image"), async (req, res) => {
+  //const tempPath = "public/photos/"+req.file.filename;
+  const photo = new Photo({
+    img: {
+      data: fs.readFileSync(
+        path.join(process.cwd() + "/public/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+    category: req.body.category,
+  });
 
-router.post('/new', upload.single("image"), async(req,res)=>{
-    //const tempPath = "public/photos/"+req.file.filename;
-    const photo = new Photo({
-        img: {
-            data: fs.readFileSync(path.join(process.cwd() + '/public/uploads/' + req.file.filename)),
-            contentType: 'image/png'
-        },
-        category: req.body.category
-    })
-    
-    try{
+  try {
+    const newPhoto = await photo.save();
+    res.redirect("/");
+  } catch {
+    res.render("newPhoto", {
+      photo: photo,
+      errorMessage: "error creating PhotoPost",
+    });
+  }
+});
 
-        const newPhoto =  await photo.save()
-        res.redirect('/')
-    }
-    catch{
-        
-        res.render('newPhoto',{
-            photo: photo,
-            errorMessage: 'error creating PhotoPost'
-        })
-    }
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const photo = await Photo.findById(req.params.id);
+    res.render("editPhoto", { photo: photo });
+  } catch {
+    res.redirect("/");
+  }
+});
+router.put("/edit/:id", upload.single("image"), async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  photo.category = req.body.category;
+  if (req.file != null) {
+    photo.img.data = fs.readFileSync(
+      path.join(process.cwd() + "/public/uploads/" + req.file.filename)
+    );
+    photo.img.contentType = "image/png";
+  }
+  try {
+    console.log(photo);
+    const newPhoto = await photo.save();
+    res.redirect("/");
+  } catch {
+    res.render("editPhoto", { photo: photo });
+  }
+});
 
-})
+router.delete("/delete/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  try {
+    await photo.remove();
+    res.redirect("/");
+  } catch {
+    res.send("Couldn't remove post");
+  }
+});
 
-router.get('/edit/:id', async(req,res)=>{
-    try{
-        const photo = await Photo.findById(req.params.id)
-        res.render('editPhoto',{photo:photo})
-    }
-    catch{
-        res.redirect('/')
-    }
-    
-})
-router.put('/edit/:id', upload.single("image"), async(req,res)=>{
-    const photo = await Photo.findById(req.params.id)
-    photo.category = req.body.category
-        if(req.file!=null){
-            photo.img.data = fs.readFileSync(path.join(process.cwd() + '/public/uploads/' + req.file.filename))
-            photo.img.contentType = 'image/png'
-        }
-    try{
-        console.log(photo)
-        const newPhoto =  await photo.save()
-        res.redirect('/')
-    }
-    catch{
-        res.render('editPhoto',{photo:photo})
-    }
-    
-})
-
-router.delete('/delete/:id', async(req,res)=>{
-    const photo = await Photo.findById(req.params.id)
-    try{
-        await photo.remove()
-        res.redirect('/')
-    }
-    catch{
-        res.send("Couldn't remove post")
-    
-    }
-})
- 
-module.exports = router
+module.exports = router;
