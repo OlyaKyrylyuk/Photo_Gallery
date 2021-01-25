e2469f0f53397f8022a7bccb33048ca10814b766
OlyaKyrylyuk <olichkakyrylyuk@gmail.com>
2021-01-25 21:15:31 +0200

    added project

---
+++ models/photo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const photoSchema = new mongoose.Schema({
+
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    },
+
+    category:{
+        type:String,
+        required: true
+    }
+    
+})
+
+  
+module.exports = mongoose.model('Photo', photoSchema)
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const db = require('./lib/db')
+const bodyParser = require('body-parser')
+var fs = require('fs')
+const methodOverride = require('method-override')
+
+
+const indexRouter = require('./routes')
+const photo = require('./models/photo')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static(__dirname+'/public'))
+app.use(bodyParser.urlencoded({limit: '30mb', extended:false}))
+app.use(bodyParser.json())
+app.use(methodOverride('_method'))
+
+
+app.use('/', indexRouter)
+
+
+
+app.listen(process.env.PORT || 3000)
